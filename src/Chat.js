4438a341ab696462a9af12f8efffe3320cfa1697
Terry Wade <terry.wade@valpo.edu>
2018-06-12 14:25:47 -0400

    Add ChatHeader

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
 
+import ChatHeader from './ChatHeader';
 class Chat extends Component {
     render(){
         return (
             <div className = "Chat">
-            Chat
+                <ChatHeader />
             </div>
         )
     }
