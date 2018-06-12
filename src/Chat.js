6fbed54d4e79904596c13af9357c7c84c2861976
Terry Wade <terry.wade@valpo.edu>
2018-06-12 15:30:06 -0400

    Add a hard coded message and submitting the form

--- src/Chat.js
+++ src/Chat.js
@@ -4,14 +4,41 @@ import ChatHeader from './ChatHeader';
 import MessageList from './MessageList';
 import MessageForm from './MessageForm';
 class Chat extends Component {
+    constructor(){
+        super();
+        this.state = {
+            messages: [
+                {
+                    id: 1,
+                    userName: 'tWade',
+                    body: 'Chatting up a storm, dawg',
+                },
+                {
+                    id: 2,
+                    userName: 'kKling',
+                    body: 'Yeah you is, b',
+                },
+            ],
+        }
+    }
     render(){
         return (
             <div className = "Chat">
                 <ChatHeader />
-                <MessageList />
-                <MessageForm />
+                <MessageList messages = {this.state.messages}/>
+                <MessageForm addMessage = {this.addMessage}/>
             </div>
         )
+    }
+
+    addMessage = () => {
+        const messages = [...this.state.messages];
+        messages.push({
+            id: Date.now(),
+            userName: "yUmm",
+            body: "i like hot dogs"
+        });
+        this.setState({messages});
     }
 }
 
--- src/MessageList.js
+++ src/MessageList.js
@@ -1,23 +1,11 @@
 import React from 'react';
 
 import Message from './Message';
-const MessageList = () => {
-    const messages = [
-        {
-            id: 1,
-            userName: 'tWade',
-            body: 'Chatting up a storm, dawg',
-        },
-        {
-            id: 2,
-            userName: 'kKling',
-            body: 'Yeah you is, b',
-        },
-    ];
-
+const MessageList = (props) => {
+    
     return (
         <div className = "MessageList">
-            {messages.map(msg => 
+            {props.messages.map(msg => 
             <Message key = {msg.id} message = {msg}/>
             )}
         </div>
