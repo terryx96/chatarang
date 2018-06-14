0a0db9fc32644c6ad84f24f4539fd5d22258e67e
Terry Wade <terry.wade@valpo.edu>
2018-06-14 15:43:18 -0400

    Add RoomForm element to be used to add rooms

--- src/RoomList.js
+++ src/RoomList.js
@@ -2,11 +2,21 @@ import React, {Component} from 'react';
 import {StyleSheet, css} from 'aphrodite';
 import Room from './Room';
 import base from "./base";
+import RoomForm from './RoomForm'
 
 class RoomList extends Component {
     state = {
         rooms: {
         },
+        showRoomForm: false,
+    }
+
+    showRoomForm = () => {
+        this.setState({showRoomForm: true});
+    }
+
+    hideRoomForm = () => {
+        this.setState({showRoomForm: false});
     }
 
     componentDidMount(){
@@ -23,9 +33,20 @@ class RoomList extends Component {
     }
 
     render(){
+        if(this.state.showRoomForm){
+            return <RoomForm addRoom = {this.addRoom} hideRoomForm = {this.hideRoomForm}/>
+        }
+        else{
         return(
             <nav className={`RoomList ${css(styles.nav)}`} >
-                <h2>Rooms</h2>
+                <div className = {`${css(styles.heading)}`}>
+                    <h2>Rooms</h2>
+                    <button
+                    className = {`${css(styles.button)}`}
+                    onClick = {this.showRoomForm}
+                    >
+                    <i className="fas fa-plus"></i></button>
+                </div>
                 <ul>
                     {
                         Object.keys(this.state.rooms).map(
@@ -39,12 +60,33 @@ class RoomList extends Component {
             </nav>
         )
     }
+    }
 }
 
 const styles = StyleSheet.create({
     nav: {
         padding: '0 1rem',
     },
+    heading: {
+        display: "flex",
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    button:{
+        border: 0,
+        backgroundColor: 'transparent',
+        outline: 0,
+        padding: 0,
+        color: 'rgba(255,255,255,.4)',
+        fontSize: '1rem',
+        cursor: 'pointer',
+        transition: 'color 0.25s ease-out',
+
+        ':hover': {
+            color: 'white',
+
+        }
+    }
 })
 
 export default RoomList;
